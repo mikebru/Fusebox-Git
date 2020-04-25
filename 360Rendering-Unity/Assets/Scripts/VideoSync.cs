3758da58fadb1797ea4ea83a0507796032ce33f7
mikebru <32722658+mikebru@users.noreply.github.com>
2020-04-25 01:40:17 -0500

    360 rendering updates, animation

---
+++ 360Rendering-Unity/Assets/Scripts/VideoSync.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Playables;
+
+public class VideoSync : MonoBehaviour
+{
+
+    public PlayableDirector TimeLineManager;
+    public VideoPlayer videoPlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(videoPlayer.canSetTime);
+        videoPlayer.Play();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Debug.Log(videoPlayer.time);
+        videoPlayer.time = TimeLineManager.time;
+
+    }
+
+
+
+
+
+}
